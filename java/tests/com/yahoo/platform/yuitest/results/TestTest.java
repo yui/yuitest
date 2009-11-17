37260621d0e9edff09f9c4babd375ea76995375b
Nicholas <nzakas@yahoo-inc.com>
2009-11-16 18:06:44 -0800

    TestCase tests

--- java/tests/com/yahoo/platform/yuitest/results/TestTest.java
+++ java/tests/com/yahoo/platform/yuitest/results/TestTest.java
@@ -1,8 +1,10 @@
 /*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+ * YUI Test Selenium Driver
+ * Author: Nicholas C. Zakas <nzakas@yahoo-inc.com>
+ * Copyright (c) 2009, Yahoo! Inc. All rights reserved.
+ * Code licensed under the BSD License:
+ *     http://developer.yahoo.net/yui/license.txt
  */
-
 package com.yahoo.platform.yuitest.results;
 
 import org.junit.After;

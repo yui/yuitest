ebea61ea62b230ff5874b9f3e360f3713ef7b0b8
Nicholas C. Zakas <nicholas.c.zakas@nczonline.net>
2009-11-28 15:08:47 -0800

    Initial port from YUI 3

---
+++ javascript/src/core/YUITest.js
@@ -0,0 +1,14 @@
+/**
+ * YUI Test Framework
+ * @module yuitest
+ */
+
+/**
+ * The root namespace for YUI Test.
+ * @class YUITest
+ * @static
+ */
+/*global YUITest*/
+YUITest = {
+    version: "@VERSION@"
+};

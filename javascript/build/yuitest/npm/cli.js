c42d8656b3d31563a2186be6b39b3e43e313d5c6
Dav Glass <davglass@gmail.com>
2012-07-24 13:12:33 -0500

    npm build

--- javascript/build/yuitest/npm/cli.js
+++ javascript/build/yuitest/npm/cli.js
@@ -137,7 +137,6 @@ YUITest.CLI = {
 
 
 
-
 /**
  * Console output that mimics logger output from YUI Test for YUI 2/3.
  * @namespace YUITest.Node.CLI
@@ -231,7 +230,6 @@ YUITest.CLI.Logger = function(){
     testRunner.subscribe(testRunner.COMPLETE_EVENT, handleEvent); 
 
 };
-
 
 
 /**
@@ -373,7 +371,6 @@ YUITest.CLI.XUnit = function(){
 };
 
 
-
 /**
  * Console output that uses TestFormat formats.
  * @namespace YUITest.Node.CLI
@@ -396,7 +393,6 @@ YUITest.CLI.Format = function(format){
     testRunner.subscribe(testRunner.COMPLETE_EVENT, handleEvent); 
 
 };
-
 /*
  * Augments the environment-specific CLI API with common functionality.
  */
@@ -494,6 +490,3 @@ YUITest.Util.mix(YUITest.CLI, {
 });
 
 YUITest.CLI.start();
-
-
-

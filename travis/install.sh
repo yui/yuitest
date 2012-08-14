6598bba93b422f74cdb716ebdee07b135a1e0c88
Dav Glass <davglass@gmail.com>
2012-08-14 15:56:52 -0500

    Adding Builder

--- travis/install.sh
+++ travis/install.sh
@@ -2,6 +2,20 @@
 
 cd "$(dirname "$0")"
 
+startDir=`pwd`
+
+
+if [ ! -d "../../builder" ]; then
+    echo "Builder is not here, cloning"
+    cd ../../
+    wait
+    git --depth=100 --quiet clone git://github.com/yui/builder.git
+    wait
+
+    cd ${startDir}
+
+fi
+
 echo "Building with ant"
 cd ../javascript
 wait

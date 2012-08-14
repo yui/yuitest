4e078cd7ca744dfe1ced1bb75962236649f8d139
Dav Glass <davglass@gmail.com>
2012-08-14 16:04:16 -0500

    Removed Ant, needs Builder

--- travis/install.sh
+++ travis/install.sh
@@ -5,19 +5,19 @@ cd "$(dirname "$0")"
 startDir=`pwd`
 
 
-if [ ! -d "../../builder" ]; then
-    echo "Builder is not here, cloning"
-    cd ../../
-    wait
-    git --depth=100 --quiet clone git://github.com/yui/builder.git
-    wait
-
-    cd ${startDir}
-
-fi
-
-echo "Building with ant"
-cd ../javascript
-wait
-ant -q -Dlint.skip=true -Dcoverage.skip=true
-wait
+#if [ ! -d "../../builder" ]; then
+#    echo "Builder is not here, cloning"
+#    cd ../../
+#    wait
+#    git --depth=100 --quiet clone git://github.com/yui/builder.git
+#    wait
+#
+#    cd ${startDir}
+#
+#fi
+#
+#echo "Building with ant"
+#cd ../javascript
+#wait
+#ant -q -Dlint.skip=true -Dcoverage.skip=true
+#wait

9bbaab21c96e7406347484c54f560a4f4d303127
Dav Glass <davglass@gmail.com>
2012-08-15 17:54:45 -0500

    Seeing if I can run a Java test suite on the Nodejs build

--- travis/test.sh
+++ travis/test.sh
@@ -21,6 +21,13 @@ RETVAL=$?
 [ $RETVAL -ne 0 ] && exit 1
 
 if [ -n "$TRAVIS" ]; then
+    echo "Setting up Java:"
+    TRAVIS_JDK_VERSION=default
+    jdk_switcher use default
+    echo -n "Java: "
+    java -version
+    echo -n "Javac: "
+    javac -version
     echo "Running Java Tests in Travis"
     cd ../java
     ant test

224882680c770ae08444ed144271056c658606c5
Dav Glass <davglass@gmail.com>
2012-08-15 10:16:23 -0500

    Added guid call to exports object

--- javascript/src/commonjs/Exports.js
+++ javascript/src/commonjs/Exports.js
@@ -14,3 +14,9 @@ YUITest.version = "@VERSION@";
 
 //backwards compatibility
 exports.YUITest = YUITest;
+
+YUITest._idx = 0;
+YUITest.guid = function(pre) {
+    var id = (new Date()).getTime() + '_' + (++YUITest._idx);
+    return (pre) ? (pre + id) : id;
+};

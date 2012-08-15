de1f3ca402e1811dd86298a6e5e4e5fbec937d32
Dav Glass <davglass@gmail.com>
2012-08-15 17:44:11 -0500

    Adding Java tests to Travis

--- travis/test.sh
+++ travis/test.sh
@@ -16,3 +16,12 @@ grover=../node_modules/.bin/grover
 echo "Grover: ${grover}"
 
 ${grover} --server ./tests/TestRunner.html -S "?autorun=true"
+
+RETVAL=$?
+[ $RETVAL -ne 0 ] && exit 1
+
+if [ -n "$TRAVIS" ]; then
+    echo "Running Java Tests in Travis"
+    cd ../java
+    ant test
+fi

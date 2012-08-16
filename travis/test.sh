51cfa4892cfc35062461d08ad601fc10c875858f
Dav Glass <davglass@gmail.com>
2012-08-16 11:36:27 -0500

    Trying with JAVA_HOME in .travis.yml

--- travis/test.sh
+++ travis/test.sh
@@ -22,9 +22,6 @@ RETVAL=$?
 
 if [ -n "$TRAVIS" ]; then
     cd ../java
-    echo "Setting up Java:"
-    ls /usr/lib/jvm/java-1.6.0-openjdk
-    export JAVA_HOME=/usr/lib/jvm/java-1.6.0-openjdk
     echo "Java Home: ${JAVA_HOME}"
     echo "ClassPath: ${CLASSPATH}"
     echo -n "Java: "
